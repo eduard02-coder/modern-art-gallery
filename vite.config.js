6dc4bee70ef71b00467cdb320ac5564990aecb91
Eduard Oganesian <eo082002@gmail.com>
2026-06-30 19:09:25 -0300

    second page loading fixed

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   // This ensures Vite hooks up the right paths for GitHub Pages
   base: '/modern-art-gallery/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+
+        // Change 'second.html' to your actual file name
+        second: resolve(__dirname, './src/html/our-location.html'),
+      },
+    },
+  },
 });
